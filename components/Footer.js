1b7b10127be3401eadd697de7b4808fcb1ca00d5
Puja <contact.pujachaudhury@gmail.com>
2022-12-05 11:35:16 -0500

    footer changes

--- components/Footer.js
+++ components/Footer.js
@@ -1,15 +1,16 @@
-// Make a footer saying 'Made by catplotlib' that links to the github repo  
+// Make a footer saying 'Made by catplotlib' that links to the github repo
 // and the catplotlib website
 
-import React from 'react';
-import styles from '../styles/footer.module.css';
+import React from "react";
+import styles from "../styles/footer.module.css";
 
 const Footer = () => {
-    return (
-        <div className={styles.container}>
-            <p>Made by catplotlib ❤️</p>
-        </div>
-    );
-    }
+  return (
+    <div className={styles.container}>
+      <p>Made by catplotlib ❤️</p>
+      <p style={{marginTop:'-0.7rem'}}>Powered by GPT3 🤖</p>
+    </div>
+  );
+};
 
 export default Footer;
